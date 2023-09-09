2112e51d92afc53abf56b82e045f72c03e3750b2
Mar <mar@strawmelonjuice.com>
2023-09-09 23:55:29 +0200

    You see, ask is indeed hoi, I would've said so myself.

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,45 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import path from "path";
 import { Logger, ILogObj } from "tslog";
 
 const tell: Logger<ILogObj> = new Logger();
 import fs from "fs";
-
+const Handlebars = require("handlebars");
 dotenv.config();
-const preloadedresponses = {
-  html: {
-    index: fs.readFileSync("./assets/html/index.html").toString()
-  }
+const vars = {
+	port: process.env.PORT,
+};
+console.log(typeof vars);
+function HandlebarsAsHTML(file: string, variables: object) {
+	const template = fs.readFileSync(file).toString();
+	// Compile said template
+	const compiled = Handlebars.compile(template);
+	const html = compiled(variables);
+	return html;
 }
-
+const preloadedresponses: object = {
+	html: {
+		index: HandlebarsAsHTML("./assets/hb/index.handlebars", vars),
+	},
+};
 const app: Express = express();
-const port = process.env.PORT;
 app.use(express.json());
+app.use(require("body-parser").urlencoded({ extended: false }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(preloadedresponses.html.index);
-  tell.log(0, "REQUEST","➡️  GET '/'");
+app.use("/assets", express.static(path.join(__dirname, "assets/public")));
+app.get("/", (req: Request, res: Response) => {
+	// res.send(preloadedresponses.html.index);
+	res.send(HandlebarsAsHTML("./assets/hb/index.handlebars", vars));
+	tell.log(0, "GET", "➡️  '/'");
+});
+app.post("*", (req, res) => {
+	req.body;
+	res.json(req.body);
+	tell.log(0, "POST", "➡️ ''");
+	console.log(req.body);
 });
 
-app.post('*', (req, res) => {
-  req.body;
-  res.json(req.body);
-});
-
-app.listen(port, () => {
-  tell.info(`⚡️ Running at http://localhost:${port}`);
+app.listen(vars.port, () => {
+	tell.info(`⚡️ Running at http://localhost:${vars.port}`);
 });
